82127ebe7b52f45919f58a0c48f35a5bdac1e984
ritg2 <ritking2@gmail.com>
2023-05-02 02:01:17 +0100

    worked on corousel still not perfect will change later

--- app.js
+++ app.js
@@ -56,19 +56,68 @@ const prevBtn = document.querySelectorAll(".btn-prev");
 sliderContainer.forEach((slide, indx) => {
   scroll(slide);
   nextBtn[indx].addEventListener("click", () => {
+    // if(screen.width < 768){
+    //   slide.scrollLeft += 300;
+    // }else if(screen.width > 768 && screen.width < 1200){
+    //   slide.scrollLeft += 300
+    // }else {
+    //   slide.width += 400
+    // }
     slide.scrollLeft += 300;
   });
-
-  console.log(nextBtn[indx])
 
   prevBtn[indx].addEventListener("click", () => {
     slide.scrollLeft -= 300;
   });
 });
 
+let scrollLeft = 0
+
+// function loop(element) {
+//   // Increment the scroll position
+//   scrollLeft += 300;
+
+//   // Reset the scroll position if it exceeds the scroll width
+//   if (scrollLeft >= element.scrollWidth) {
+//     scrollLeft = 0;
+//   }
+
+//   // Set the new scroll position
+//   element.scrollLeft = scrollLeft;
+
+//   // Call loop() again after a short delay to animate the scroll
+//   setTimeout(loop, 10);
+// }
+
+setInterval(() => {
+  autoSlide(sliderContainer[0]);
+}, 5000);
+
+setInterval(() => {
+  autoSlide(sliderContainer[1]);
+}, 6000);
+
+setInterval(() => {
+  autoSlide(sliderContainer[2]);
+}, 7000);
+
+setInterval(() => {
+  autoSlide(sliderContainer[3]);
+}, 5000);
+
+function autoSlide(element) {
+  if (element.scrollLeft === element.scrollWidth - element.offsetWidth) {
+    element.scrollLeft = 0;
+    console.log("hello");
+  } else {
+    element.scrollLeft += 300;
+    console.log(element.scrollLeft);
+    console.log(element.scrollWidth - element.offsetWidth);
+  }
+}
 
 const showOnPx = 300;
-const navbar = document.querySelector("nav")
+const navbar = document.querySelector("nav");
 
 const scrollContainer = () => {
   return document.documentElement || document.body;
@@ -76,8 +125,8 @@ const scrollContainer = () => {
 
 document.addEventListener("scroll", () => {
   if (scrollContainer().scrollTop > showOnPx) {
-    navbar.classList.add("change")
+    navbar.classList.add("change");
   } else {
-    navbar.classList.remove("change")
+    navbar.classList.remove("change");
   }
-})
+});
